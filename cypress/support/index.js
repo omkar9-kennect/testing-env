0e113d0a0867c0194026458855ba1a766c91293c
omkar9-kennect <132801192+omkar9-kennect@users.noreply.github.com>
2023-11-29 15:43:25 +0530

    first-commit

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,16 +1,3 @@
 import './commands.js'
 import './e2e.js'
 import './component.js'
-
-import 'cypress-file-upload';
-Cypress.Commands.add('upload_file', (fileName, fileType = ' ', selector) => {
-cy.get(selector).then(subject => {
-cy.fixture(fileName, 'base64').then(content => {
-const el = subject[0];
-const testFile = new File([content], fileName, { type: fileType });
-const dataTransfer = new DataTransfer();
-dataTransfer.items.add(testFile);
-el.files = dataTransfer.files;
-});
-});
-});
